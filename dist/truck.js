d1e2cf5975fd9abdc5ee0c5006bacf9d762b9ba0
Robert Biggswobbabits <rbiggs@ymail.com>
2016-01-06 13:55:27 -0800

    Fixed $.AdjustNavbarLayout bug.
    Needed to delay execution a few milliseconds to enable capturing true
    width of nav siblings to adjust h1 margins.

--- dist/truck.js
+++ dist/truck.js
@@ -8020,15 +8020,12 @@
       AdjustNavbarLayout: function(screen) {
         if (!$('link[href*=ios]')[0]) return;
         screen = $(screen);
-        var siblings = screen.find('h1').siblings();
+        var h1 = screen.find('h1');
+        var siblings = h1.siblings();
         var whichSide;
         var oppositeSide;
         var rtl = ($('html').attr('dir') === 'rtl');
         var amount = 0;
-        var padding = 20;
-        var oppositeAmount = 25;
-        var h1 = screen.find('h1');
-        var navWidth = screen.find('nav').width();
         var hidden = false;
         var visibleSibling;
 
@@ -8075,8 +8072,8 @@
         };
 
         function handleOneSibling(sib) {
-          var sibling = sib || h1.siblings().eq(0);
-          amount = sibling.width();
+          var sibling = sib || h1.siblings();
+          amount = sibling[0].clientWidth;
           if (sibling.is(':first-child')) {
             whichSide = 'margin-right';
             oppositeSide = 'margin-left';
@@ -8093,9 +8090,11 @@
             }
           }
         }
+
         // If one sibling:
         if (siblings.length === 1) {
           handleOneSibling();
+          console.log(siblings[0].clientWidth)
 
           // If two siblings:
         } else if (siblings.length === 2) {
@@ -8120,13 +8119,14 @@
         }
         var props = {};
         props[whichSide] = amount;
-        props[oppositeSide] = 0;
+        // props[oppositeSide] = 0;
         var sibwidth = 0;
         if (siblings.size()) {
           siblings.forEach(function(item) {
-            sibwidth += $(item).width();
+            sibwidth += $(item)[0].clientWidth;
           });
         }
+        // alert(amount)
         var headerWidth = screen.find('nav').width() / 2;
         if ((sibwidth + 20) > headerWidth) {
           h1.css({
@@ -8138,8 +8138,10 @@
         }
       }
     });
-    $('screen').forEach(function(screen) {
-      $.AdjustNavbarLayout(screen);
+    setTimeout(function() {
+      $('screen').forEach(function(screen) {
+        $.AdjustNavbarLayout(screen);
+      });
     });
   });
 })();
@@ -10164,6 +10166,11 @@
       } else if ($('body').hasClass('isAndroid')) {
         androidBusy(options);
       } else if ($('body').hasClass('isiOS')) {
+        iOSBusy(options);
+      }
+    }
+  });
+})();'isiOS')) {
         iOSBusy(options);
       }
     }
--- src/truck-body/navbar.js
+++ src/truck-body/navbar.js
@@ -10,15 +10,12 @@
       AdjustNavbarLayout: function(screen) {
         if (!$('link[href*=ios]')[0]) return;
         screen = $(screen);
-        var siblings = screen.find('h1').siblings();
+        var h1 = screen.find('h1');
+        var siblings = h1.siblings();
         var whichSide;
         var oppositeSide;
         var rtl = ($('html').attr('dir') === 'rtl');
         var amount = 0;
-        var padding = 20;
-        var oppositeAmount = 25;
-        var h1 = screen.find('h1');
-        var navWidth = screen.find('nav').width();
         var hidden = false;
         var visibleSibling;
 
@@ -65,8 +62,8 @@
         };
 
         function handleOneSibling(sib) {
-          var sibling = sib || h1.siblings().eq(0);
-          amount = sibling.width();
+          var sibling = sib || h1.siblings();
+          amount = sibling[0].clientWidth;
           if (sibling.is(':first-child')) {
             whichSide = 'margin-right';
             oppositeSide = 'margin-left';
@@ -83,9 +80,11 @@
             }
           }
         }
+
         // If one sibling:
         if (siblings.length === 1) {
           handleOneSibling();
+          console.log(siblings[0].clientWidth)
 
           // If two siblings:
         } else if (siblings.length === 2) {
@@ -110,13 +109,14 @@
         }
         var props = {};
         props[whichSide] = amount;
-        props[oppositeSide] = 0;
+        // props[oppositeSide] = 0;
         var sibwidth = 0;
         if (siblings.size()) {
           siblings.forEach(function(item) {
-            sibwidth += $(item).width();
+            sibwidth += $(item)[0].clientWidth;
           });
         }
+        // alert(amount)
         var headerWidth = screen.find('nav').width() / 2;
         if ((sibwidth + 20) > headerWidth) {
           h1.css({
@@ -128,8 +128,10 @@
         }
       }
     });
-    $('screen').forEach(function(screen) {
-      $.AdjustNavbarLayout(screen);
+    setTimeout(function() {
+      $('screen').forEach(function(screen) {
+        $.AdjustNavbarLayout(screen);
+      });
     });
   });
 })();
